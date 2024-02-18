89bcbac67ea05bab884db97e7a884bb607e89c27
CharlesSampaio-CRS <charles.sampaio@inter.co>
2024-02-18 17:23:21 -0300

    Tratativas Contrato de Peticao simples

--- constracts/Petition.sol
+++ constracts/Petition.sol
@@ -12,10 +12,25 @@ contract Petition{
         nameCount = 0;
     }
 
-    function registerName(string memory _name) {
+    function registerName(string memory _name) public {
+        if(nameCount < 3){
         names[nameCount] = _name;
         //Se nao incrementar ++ o nome sempre sera sobreposto 
         nameCount++;
+        }
+        return "Petition is full";
+    }
+
+    function getName(uint256 _id) public view returns (string memory) {
+        if(_id < 3){
+            return names[_id];
+        }
+        return "Error: Index out of bounds";
+
+    }
+
+    function isPetitionFull() public view returns(bool) {
+         return nameCount >= 3;
     }
 
 }
